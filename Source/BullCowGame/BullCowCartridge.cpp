8f3d257190b0186d753f9c9906c7ab70f778c6af
joseeduardogaytan <joseEduardoGaytan@gmail.com>
2020-04-23 14:42:28 -0500

    Game over and restart behaviour

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,62 +5,93 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    // Welcoming the Player
-    // TEXT macro to encode unreal string
-    PrintLine(TEXT("Welcome to Bulls Cows Game!"));
-    // Ask the user to input the guess
-    PrintLine(TEXT("Guess the 4 letter word!")); // TODO: Change it for a not hardcoded variable
-    PrintLine(TEXT("Press enter to continue..."));
-
     // Seting Up Game
-    // Set the hidden word
-    this->HiddenWord = TEXT("bird"); // Unreal can encode the string correctly;
+    SetupGame();
 
-    // TODO: Set lives
+    //PrintLine(FString::Printf(TEXT("The Hidden Word is: %s"), *HiddenWord)); // Debug line we can turn it off/on as we need
+    PrintLine(TEXT("The Hidden Word is: %s.\nIt is %i characters long"), *HiddenWord, HiddenWord.Len()); // Debug line we can turn it off/on as we need -- no need of Printf
+        
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {    
     ClearScreen();
-    PrintLine(Input);
 
-    // TODO: Validation    
-    // TODO: Check if isogram
-    // bool bIsIsogram = this->IsIsogram(Input);
-    // TODO: check if correct number of characters
-    // bool bHasCorrectNumberOfChars = this->CheckNumberOfChars(this->HiddenWord, Input);
-
-    // Do we need to surround this on a loop? or global variables will be enough?
-    // Checking Player Guess
-    if (Input == this->HiddenWord) 
+    // if game is over then do ClearScreen and SetupGame()
+    if (bGameOver) 
     {
-        PrintLine(TEXT("Congratulations you win!"));
+        SetupGame();
     }
-    else 
+    // else check the Player Guess
+    else
     {
-        // TODO: Subtract life
-        // this->UserLifes--;
-        // If the user run out of lives then print a message telling the input was not correct and break the loop
-        // Ask if the user wants to play again
-        // if don't then exit the game
-        // else start again
+        PrintLine(TEXT("Your guess is %s"), *Input);
 
-        // else Sorry but your guess was not correct, please try again
-        PrintLine(TEXT("Sorry but your guess is not correct, you lose!"));
-    }
-        
-    //while (false) 
-    //{
-    //    FString HiddenWord = TEXT("bird"); // Unreal can encode the string correctly
-    //    ClearScreen();
-    //    if (Input == HiddenWord) 
-    //    {
-    //        // Congratulations you win!
-    //    }
-    //    else 
-    //    {
-    
-    //    }
-    //}   
+        // Do we need to surround this on a loop? or global variables will be enough?
+        // Checking Player Guess
+        if (Input == this->HiddenWord)
+        {
+            PrintLine(TEXT("Congratulations you win!"));   
+            EndGame();
+        }
+        else
+        {
+            // TODO: Validation    
+            // TODO: Check if isogram
+            // bool bIsIsogram = this->IsIsogram(Input);
+            // check if correct number of characters
+            bool bHasCorrectNumberOfChars = this->CheckWordLength(this->HiddenWord, Input);
 
+            if (!bHasCorrectNumberOfChars)
+            {
+                PrintLine(TEXT("Sorry but the Hidden Word is %i characters long, try again!"), HiddenWord.Len());
+            }
+            else
+            {
+                // TODO: Subtract life
+                // this->UserLifes--;
+                // If the user run out of lives then print a message telling the input was not correct and break the loop
+                // Ask if the user wants to play again
+                // if don't then exit the game
+                // else start again
+
+                // else Sorry but your guess was not correct, please try again
+                PrintLine(TEXT("Sorry but your guess is not correct, please try again!"));
+            }
+             
+            EndGame();
+        }
+    }       
+}
+
+void UBullCowCartridge::SetupGame()
+{
+    // Set the hidden word
+    this->HiddenWord = TEXT("bird"); // Unreal can encode the string correctly;
+
+    // Set lives
+    this->UserLives = 4;
+    bGameOver = false;
+
+    WelcomePlayer();
+}
+
+bool UBullCowCartridge::CheckWordLength(const FString HiddenWord, FString Input)
+{
+    return HiddenWord.Len() == Input.Len();
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to play again\nOtherwise please hit Esc"));
+}
+
+void UBullCowCartridge::WelcomePlayer()
+{
+    // TEXT macro to encode unreal string
+    PrintLine(TEXT("Welcome to Bulls Cows Game!"));
+    // Ask the user to input the guess
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len()); // TODO: Change it for a not hardcoded variable
+    PrintLine(TEXT("Press enter to continue..."));
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -14,9 +14,15 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	virtual void SetupGame();
+	virtual bool CheckWordLength(const FString HiddenWord, FString Input);
+	virtual void WelcomePlayer();
+	virtual void EndGame();
 
 	// Your declarations go below!
 	private:
 	FString HiddenWord;
+	int32 UserLives;
+	bool bGameOver;
 	
 };
